4fa263495100786bdb2993169917ac574797245a
pankaj-404 <pankajbhayre@gmial.com>
2023-12-19 19:41:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+import "./index.css";
+import store from "./store/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
